cdbcacac13b7ccdda9ffa4536eb07dc68d67ae41
kunalshaw79 <95361750+kunalshaw79@users.noreply.github.com>
2024-01-10 05:02:32 +0530

    used  row component use

--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -40,30 +40,7 @@ export default function Table() {
                         </thead>
                         <tbody class="bg-white">
                             <Row id="#1" name="Kunal" title="BMS, intellegent battery management system" department="physics" status="approved by dean siric" lastmodified="12 Dec" submittedon="1 Dec" view_details="click here"></Row>
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                                        <div class="flex items-center">
-                                            <div>
-                                                <div class="text-sm leading-5 text-gray-800">#1</div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                                        <div class="text-sm leading-5 text-blue-900">Damilare Anjorin</div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">damilareanjorin1@gmail.com</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">+2348106420637</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
-                                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                        <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                        <span class="relative text-xs">active</span>
-                                    </span>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">September 12</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
-                                        <button class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">View Details</button>
-                                    </td>
-                                </tr>
+                    {/* add rows like this expect hard coding */}
                                <tr>
                                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
                                         <div class="flex items-center">
