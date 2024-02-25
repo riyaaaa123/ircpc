6e919fd14384e15114fe198aa666bde60e85be3c
kunalshaw79 <95361750+kunalshaw79@users.noreply.github.com>
2024-02-26 00:11:44 +0530

    channeli

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -9,7 +9,7 @@ export default function Nav() {
   <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 white:bg-gray-800">
     <div className="flex flex-wrap justify-between items-center">
       <div className="flex justify-start items-center">
-      <button id="toggleSidebar" aria-expanded="true" aria-controls="sidebar" class="hidden p-2 mr-3 text-gray-600 rounded cursor-pointer lg:inline hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
+      <button id="toggleSidebar" aria-expanded="true" aria-controls="sidebar" className="hidden p-2 mr-3 text-gray-600 rounded cursor-pointer lg:inline hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h14M1 6h14M1 11h7"/>
           </svg>
